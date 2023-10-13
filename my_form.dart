767066e6af37a365411f764dd8fd368f316a09c2
saria4558 <128865849+saria4558@users.noreply.github.com>
2023-10-14 04:52:03 +0700

    Update my_form.dart

--- my_form.dart
+++ my_form.dart
@@ -1,52 +1,214 @@
 import 'package:flutter/material.dart';
+import 'user_data.dart';
 
-class Second extends StatelessWidget {
-  final String Fullname;
-  final String email;
-  final String phoneNumber;
-  final String personalID;
-  final String address;
+class PersonalForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Personal Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Personal Form',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+          actions: [
+            IconButton(
+              onPressed: () {
+                // Fungsi yang ingin Anda tambahkan saat ikon ditekan
+              },
+              icon: Icon(
+                Icons.description,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+        body: CardList(),
+      ),
+    );
+  }
+}
 
-  Second({
-    required this.Fullname,
-    required this.email,
-    required this.phoneNumber,
-    required this.personalID,
-    required this.address,
-  });
+class CardList extends StatefulWidget {
+  @override
+  _CardListState createState() => _CardListState();
+}
+
+class _CardListState extends State<CardList> {
+  String fullName = '';
+  String email = '';
+  String phoneNumber = '';
+  String personalID = '';
+  String address = '';
+  String selectedDate = '';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'List Personal Data',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16.0),
-        children: [
-          Container(
-            padding: EdgeInsets.all(12.0),
-            margin: EdgeInsets.only(bottom: 12.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black, width: 1),
-              borderRadius: BorderRadius.circular(5.0),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(Fullname),
-                Text(
-                  email,
-                  style: TextStyle(fontSize: 14, color: Colors.blue),
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Full Name'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1),
+                  borderRadius: BorderRadius.circular(5.0),
                 ),
-                Text(address),
-              ],
-            ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Value',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      fullName = value;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Email'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Value',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(height: 18.0),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 1),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Nomor Telepon',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            phoneNumber = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8.0),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      width: 100,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 1),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: Center(
+                        child: Text('Verify'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Personal ID Number'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Value',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      personalID = value;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('Address'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter your text here',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      address = value;
+                    });
+                  },
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => Second(
+                      Fullname: fullName,
+                      email: email,
+                      phoneNumber: phoneNumber,
+                      personalID: personalID,
+                      address: address,
+                    ),
+                  ));
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Submit'),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
